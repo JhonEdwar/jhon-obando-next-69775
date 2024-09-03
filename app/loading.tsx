38d62ff04f8231b94a9bd99e20b0cf609a6318d7
Jhon Edwar <jhonedwar192@gmail.com>
2024-09-03 08:27:46 -0500

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Loading = () => {
+    return (
+        <div className='w-full h-full min-h-screen flex justify-center items-center text-white'> 
+            Cargando ....
+        </div>
+    )
+}
+
+export default Loading
